858aa2f87862eda712d025c028b45280b8326964
jhsilvaz13 <jhsilvaz@unal.edu.co>
2024-03-22 08:44:46 -0500

    Created map page

--- lib/src/features/root/my_home.dart
+++ lib/src/features/root/my_home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dev/src/common/widgets/bottom_nav_bar.dart';
 import 'package:flutter_dev/src/features/home/screens/home_screen.dart';
+import 'package:flutter_dev/src/features/map/screens/map_screen.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key}) : super(key: key);
@@ -11,10 +12,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
-  static List<Widget> _widgetOptions = <Widget>[
-    HomePage(),
-    HomePage(),
-  ];
+  static List<Widget> _widgetOptions = <Widget>[HomePage(), MapPage()];
 
   void _onItemTapped(int index) {
     setState(() {
